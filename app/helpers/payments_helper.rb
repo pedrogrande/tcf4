e94261ce054c9088de9e62d20bb9cfbae5bd147d
Pete Argent <pete@thecoderfactory.com>
2014-06-10 21:05:52 +1000

    fix special error

--- app/helpers/payments_helper.rb
+++ app/helpers/payments_helper.rb
@@ -1,7 +1,11 @@
 module PaymentsHelper
 	def enrolment_price(enrolment)
 		@enrolment = enrolment
-		@special = Special.find(@enrolment.special_id)
-		(@enrolment.popular_programme.price - @special.dollar_discount) * 100
+		if @enrolment.special
+			@special = Special.find(@enrolment.special_id)
+			(@enrolment.popular_programme.price - @special.dollar_discount) * 100
+		else
+			@enrolment.popular_programme.price * 100
+		end
 	end
 end
