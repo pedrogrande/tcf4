fa5c6f498d76c42761fdb954f9d68e025559d7c5
Pete Argent <pete@thecoderfactory.com>
2014-06-10 23:45:19 +1000

    fixed about page

--- app/controllers/about_controller.rb
+++ app/controllers/about_controller.rb
@@ -4,5 +4,8 @@ class AboutController < ApplicationController
 	impressionist
   def index
   	@profiles = Profile.all
+  	@weekend_programme = WeekendProgramme.future_by_date_ascending.first
+  	@latest_post = Post.published_in_reverse_chron_order.first
+  	@popular_post = Post.popular_posts.first
   end
 end
